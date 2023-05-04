a82447dd3ac048c91999d8a3d021fcc5357d75a7
Davide <davide.sirico@gmail.com>
2023-05-04 19:58:39 +0200

    core server and db working

---
+++ __init__.py
@@ -0,0 +1,30 @@
+import os
+
+from flask import Flask
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True) # load the instance config, if it exists, when not testing
+    else:
+        app.config.from_mapping(test_config)
+    
+    try:
+        os.makedirs(app.instance_path) # ensure the instance folder exists
+    except OSError:
+        pass
+
+    from . import db
+    db.init_app(app)
+
+    from . import auth
+    app.register_blueprint(auth.bp)
+    
+    app.add_url_rule('/', endpoint='index')
+
+    return app
